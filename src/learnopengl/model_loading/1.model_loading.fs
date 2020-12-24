9dba86c6c0f1d206cb3a10e5691e62c83516958f
pgeorge1324 <985471009@qq.com>
2020-12-24 10:14:08 +0800

    fix(model_loading package): 顶点着色器新增法线和片段点位输出；引入commitizen规范commit msg;
    BREAKING CHANGE: 引入commitizen 规范commit msg

--- src/learnopengl/model_loading/1.model_loading.fs
+++ src/learnopengl/model_loading/1.model_loading.fs
@@ -2,12 +2,11 @@
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
 
 uniform sampler2D texture_diffuse1;
 uniform sampler2D texture_specular1;
-
-in vec3 Normal;
-in vec3 FragPos;
 
 uniform vec3 lightPos;
 uniform vec3 lightColor;
